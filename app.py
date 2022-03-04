538a14a92eea71109f037f5df2ff78d2b6621f39
Patricio Garbino <pato.garbino@gmail.com>
2022-03-04 15:00:59 -0300

    last

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import requests
 
+CSS = """
+h1 {
+    color: blue;
+}
+"""
+
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
 '''
 # TaxiFareModel Prediction
 '''
 
 st.markdown('''
-Please insert the required inputs
+### Please insert the required inputs
 ''')
 
 
@@ -23,26 +31,36 @@ lat2 = st.text_input(label='Dropoff Latitude')
 #passenger count
 cont = st.text_input(label='Number of passengers')
 
+if date and long1 and lat1 and long2 and lat2 and cont:
 
+    url = 'https://taxifare.lewagon.ai/predict'
 
-url = 'https://taxifare.lewagon.ai/predict?\
-pickup_datetime='+date+'&\
-pickup_longitude='+long1+'&\
-pickup_latitude='+lat1+'&\
-dropoff_longitude='+long2+'&\
-dropoff_latitude='+lat2+'&\
-passenger_count='+cont
+    params = { 'pickup_datetime':date,
+            'pickup_longitude':long1,
+            'pickup_latitude': lat1,
+            'dropoff_longitude': long2,
+            'dropoff_latitude':lat2,
+            'passenger_count': cont
+    }
 
-params = { 'pickup_datetime':date,
-          'pickup_longitude':long1,
-          'pickup_latitude': lat1,
-          'dropoff_longitude': long2,
-          'dropoff_latitude':lat2,
-          'passenger_count': cont
-}
+    response = requests.get(url,params=params).json()['fare']
 
-response = requests.get(url).json()
-response = round (response['fare'],2)
-texto = 3
-st.text(url)
-st.markdown(f'### The Fare prediction is:\n### {response}')
+    st.markdown(f'### The Fare prediction is: USD {response:.2f}\n')
+
+    import folium
+
+    from streamlit_folium import folium_static
+
+    m = folium.Map(location=[(float(long1)+float(long2))/2, (float(lat1)+float(lat2))/2], zoom_start=11)
+
+    folium.Marker(location=[float(long1), float(lat1)],
+                popup='Pickup',
+                icon=folium.Icon(color="blue", icon="info-sign"),
+                ).add_to(m)
+
+    folium.Marker(location=[float(long2), float(lat2)],
+                popup='Dropoff',
+                icon=folium.Icon(color="blue", icon="info-sign"),
+                ).add_to(m)
+
+    folium_static(m)
